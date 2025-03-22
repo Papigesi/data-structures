d17a96287af1fcd60db673199ac9a37a2e3a9077
Papigesi <mtnbngllll@icloud.com>
2025-03-22 21:03:55 +0300

    Data structures assignments added

---
+++ Lab3/3.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Iste
+{
+    class CityPair
+    {
+        public string[] cityNames;
+        public int point;
+        public CityPair()
+        {
+            cityNames = new string[2];
+            point = 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Istanbul", "Roma", "Paris", "Madrid", "New York", "Moskova", "Londra", "Pekin" };
+
+            string[] trip = tripPlan(cities, r);
+            CityPair[] combinationCities = combination(trip, r);
+
+            listCities(combinationCities);
+            Console.WriteLine();
+            listIstanbul(combinationCities, "Istanbul");
+            Console.ReadKey();
+        }
+
+        static Random r = new Random();
+        static string[] tripPlan(string[] cities, Random r)
+        {
+            string[] citiesCopy = new string[cities.Length];
+            Array.Copy(cities, citiesCopy, cities.Length);
+
+            string[] tripPlan = new string[4];
+            int maxValue = cities.Length;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int randomNumber = r.Next(0, maxValue);
+                tripPlan[i] = citiesCopy[randomNumber];
+
+                for (int j = randomNumber; j < citiesCopy.Length - 1; j++)
+                {
+                    citiesCopy[j] = citiesCopy[j + 1];
+                }
+
+                maxValue--;
+            }
+
+            return tripPlan;
+        }
+
+        static CityPair[] combination(string[] tripPlan, Random r)
+        {
+            CityPair[] cityCombinations = new CityPair[6];
+            int count = 0;
+
+            for (int i = 0; i < tripPlan.Length - 1; i++)
+            {
+                for (int j = i + 1; j < tripPlan.Length; ++j)
+                {
+                    CityPair cityPair = new CityPair();
+                    cityPair.cityNames[0] = tripPlan[i];
+                    cityPair.cityNames[1] = tripPlan[j];
+                    cityPair.point = r.Next(0, 101);
+                    cityCombinations[count] = cityPair;
+                    count++;
+                }
+            }
+
+            return cityCombinations;
+        }
+
+        static void listCities(CityPair[] cityCombinations)
+        {
+            Console.WriteLine("Tüm şehir çiftleri:");
+            Console.WriteLine("-----------------------------------------------");
+            for (int i = 0; i < cityCombinations.Length; i++)
+            {
+                Console.WriteLine("Şehir çifti {0} - {1}  Puan: {2}", cityCombinations[i].cityNames[0], cityCombinations[i].cityNames[1], cityCombinations[i].point);
+            }
+        }
+
+        static void listIstanbul(CityPair[] cityCombinations, string city)
+        {
+            Console.WriteLine("İçinde {0} şehrini barındıran şehir çiftleri:", city);
+            Console.WriteLine("-----------------------------------------------");
+            for (int i = 0; i < cityCombinations.Length; i++)
+            {
+                if (cityCombinations[i].cityNames[0].Equals(city) || cityCombinations[i].cityNames[1].Equals(city))
+                {
+                    Console.WriteLine("Şehir çifti {0} - {1}  Puan: {2}", cityCombinations[i].cityNames[0], cityCombinations[i].cityNames[1], cityCombinations[i].point);
+                }
+            }
+        }
+    }
+}
---
+++ Lab4/4.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class District
+    {
+        public string districtName;
+        public int distance;
+
+        public District(string districtName, int distance)
+        {
+            this.districtName = districtName;
+            this.distance = distance;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] districtNames = { "Balçova", "Bornova", "Bayraklı", "Buca", "Çiğli", "Gaziemir", "Güzelbahçe", "Karşıyaka", "Konak", "Narlıdere", "Aliağa", "Bayındır", "Bergama", "Beydağ", "Çeşme", "Dikili", "Foça", "Karaburun", "Kemalpaşa", "Kınık", "Kiraz", "Menderes", "Menemen", "Ödemiş", "Seferihisar", "Selçuk", "Karabağlar", "Tire", "Torbalı", "Urla" };
+            int[] distances = { 14, 4, 9, 10, 11, 8, 30, 6, 0, 17, 53, 78, 102, 141, 88, 105, 64, 107, 24, 119, 142, 23, 29, 113, 52, 76, 25, 84, 46, 42 };
+
+            ArrayList al = new ArrayList();
+            List<District> districts;
+            District district;
+
+            int counter = 0;
+            int exponent = 0;
+
+            while (counter < districtNames.Length)
+            {
+                districts = new List<District>();
+                int listLength = (int)Math.Pow(2, exponent);
+
+                for (int i = 0; i < listLength && counter < districtNames.Length; i++)
+                {
+                    if (counter < districtNames.Length)
+                    {
+                        district = new District(districtNames[counter], distances[counter]);
+                        districts.Add(district);
+                        counter++;
+                    }
+                }
+                al.Add(districts);
+                exponent++;
+            }
+
+            foreach (List<District> l in al)
+            {
+                foreach (District d in l)
+                {
+                    Console.WriteLine("İlçe adı: {0} --- Merkeze uzaklık: {1}", d.districtName, d.distance);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
